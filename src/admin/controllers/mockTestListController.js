870bc2ec311edfe56368026aa91a20750cb71a72
Adil Hussain <silver@Adils-MacBook-Air.local>
2024-12-18 12:33:35 +0530

    mockTest Api redesigned now can upload image

--- src/admin/controllers/mockTestListController.js
+++ src/admin/controllers/mockTestListController.js
@@ -1,11 +1,29 @@
 const constant = require("../../config/constant");
 const { sendResponse } = require("../../config/helper");
 const MockTestList = require("../Models/mockTestList");
+const { uploadToCloudinary } = require("../../helper/cloudinary");
 
 module.exports = {
   createMockTest: async (req, res) => {
     try {
-      const { testName, imgUrl, testType } = req.body;
+      const { testName, testType: testTypeString } = req.body;
+      const testType = testTypeString.split(",").map((item) => item.trim());
+      let imgUrl = "";
+      if (req.file) {
+        result = await uploadToCloudinary(
+          req.file.buffer,
+          req.file.originalname
+        );
+        if (result?.http_code == 400)
+          return sendResponse(
+            "error during upload file",
+            res,
+            constant.CODE.INPUT_VALIDATION,
+            {},
+            0
+          );
+        imgUrl = result.secure_url;
+      }
       const isTestExist = await MockTestList.findOne({ testName });
       if (!isTestExist) {
         const mockTest = new MockTestList({
--- src/admin/routes/mockTest.js
+++ src/admin/routes/mockTest.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../../helper/jwtAuth");
+const upload = require("../../helper/fileUpload");
 const mockTest = require("../controllers/mockTestListController");
 const question = require("../controllers/questionController");
 
-router.post("/create-mock-test", authenticate, mockTest.createMockTest);
+router.post(
+  "/create-mock-test",
+  upload.single("imgFile"),
+  authenticate,
+  mockTest.createMockTest
+);
 router.get("/get-test-list", authenticate, mockTest.getTestList);
 router.delete("/delete-test-list/:id", authenticate, mockTest.deleteTestList);
 router.get("/get-questions", authenticate, question.getQuestion);
