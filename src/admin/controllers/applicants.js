6239c29251bb92925f9eebd5ab4a9446f719fbed
Adil Hussain <silver@Adils-MacBook-Air.local>
2025-04-02 11:49:28 +0530

    filter added

--- src/admin/controllers/applicants.js
+++ src/admin/controllers/applicants.js
@@ -4,15 +4,103 @@ const constant = require("../../config/constant");
 
 module.exports = {
   getAllApplicants: async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search || "";
+    const fromDate = req.query.fromDate || "";
+    const toDate = req.query.toDate || "";
+    const jobTitle = req.query.jobTitle || "";
+    const categoryName = req.query.categoryName || "";
+    const subCategoryName = req.query.subCategoryName || "";
+    const status = req.query.status || "";
+
+    const matchConditions = {};
+
+    if (jobTitle) {
+      matchConditions["jobpost.jobTitle"] = { $regex: jobTitle, $options: "i" };
+    }
+    if (subCategoryName) {
+      matchConditions["jobpost.subCategoryName"] = {
+        $regex: subCategoryName,
+        $options: "i",
+      };
+    }
+    if (categoryName) {
+      matchConditions["jobpost.categoryName"] = {
+        $regex: categoryName,
+        $options: "i",
+      };
+    }
+    if (search) {
+      matchConditions["$or"] = [
+        { "user.name": { $regex: search, $options: "i" } },
+        { "user.cin_number": { $regex: search, $options: "i" } },
+        { "jobpost.jobTitle": { $regex: search, $options: "i" } },
+        { "jobpost.categoryName": { $regex: search, $options: "i" } },
+        { "jobpost.subCategoryName": { $regex: search, $options: "i" } },
+        { status: { $regex: search, $options: "i" } },
+      ];
+    }
+    if (fromDate && toDate) {
+      const from = new Date(fromDate);
+      const to = new Date(toDate);
+      to.setHours(23, 59, 59, 999);
+      matchConditions.createdAt = { $gte: from, $lte: to };
+    }
+    if (status) {
+      matchConditions["status"] = status;
+    }
+
+    const pipeline = [
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      { $unwind: "$user" },
+      {
+        $lookup: {
+          from: "jobposts",
+          localField: "jobpostId",
+          foreignField: "_id",
+          as: "jobpost",
+        },
+      },
+      { $unwind: "$jobpost" },
+      {
+        $project: {
+          "user.password": 0,
+          "user.__v": 0,
+          "jobpost.jobSector": 0,
+          "jobpost.companyName": 0,
+          "jobpost.city": 0,
+          "jobpost.locality": 0,
+          "jobpost.interviewAddress": 0,
+          "jobpost.aboutCompany": 0,
+          "jobpost.contactPerson": 0,
+          "jobpost.contactNumber": 0,
+          "jobpost.companyLogo": 0,
+          "jobpost.isSaved": 0,
+          "jobpost.__v": 0,
+        },
+      },
+      { $match: matchConditions },
+      { $skip: (page - 1) * limit },
+      { $limit: limit },
+    ];
+
     try {
-      const applicants = await Applicant.find()
-        .populate("userId")
-        .populate("jobpostId");
+      const applicants = await Applicant.aggregate(pipeline);
+      const totalApplicants = applicants.length;
+
       return sendResponse(
         "Applicants fetched successfully",
         res,
         constant.CODE.SUCCESS,
-        { applicants },
+        { applicants, totalApplicants },
         0
       );
     } catch (error) {
@@ -25,6 +113,7 @@ module.exports = {
       );
     }
   },
+
   updateApplicantStatus: async (req, res) => {
     const { userId, jobpostId, permit } = req.body;
     try {
